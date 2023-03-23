033891bfd6aadafc261e54f0d4aaffb5180762d0
Mirabelle89 <128680019+Mirabelle89@users.noreply.github.com>
2023-03-23 15:47:20 +0800

    Add files via upload

---
+++ madilb.py
@@ -0,0 +1,28 @@
+
+adj1=input("Adjective: ")
+n1=input("Noun: ")
+name=input("Name: ")
+adj2=input("Adjective: ")
+n2=input("Noun: ")
+adj3=input("Adjective: ")
+n3=input("Noun: ")
+villain=input("Villain: ")
+adj4=input("Adjective: ")
+n4=input("Noun: ")
+adj5=input("Adjective: ")
+n5=input("Noun: ")
+verb=input("Verb: ")
+adj6=input("Adjective: ")
+n6=input("Noun: ")
+adj7=input("Adjective: ")
+n7=input("Noun: ")
+verb2=input("Verb: ")
+madlib=f"Once upon a time,there was a {adj1} {n1}named {name}.\n" \
+       f"{name} was know throughout the land for their incredible {adj2} {n2} skillls.\n"\
+       f"One day,a wicked {adj3} {n3} named {villain}came to town and challenged {name} to a {adj4} {n4} competition.\n" \
+       f"{name} agreed,but little did they know,{villain} was actually a {adj5} {n5} in dis guise!\n" \
+       f"The {adj4} {n4} competition turned out to be a trap,and {name} found themselves {verb} into a {adj6} {n6} with no way out.\n" \
+       f"Luckily,{name} had a secret weapon-a {adj7} {n7} that they had been saving for just such an occasion.\n" \
+       f"With one swift {verb2},{name} managed to defeat {villain} and escape from the {adj6} {n6}.\n" \
+       f"From that day forward,{name} was known not just for their incredible {adj2} {n2} skill,but also for their bravery in the face of {adj3} {n3}."
+print(madlib)
